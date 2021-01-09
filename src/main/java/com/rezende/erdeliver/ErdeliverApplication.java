c640f98a5e4a7280c225c1507c1c48a01fa18f58
Ernando <ernando.rezende@dtidigital.com.br>
2021-01-09 15:12:34 -0300

    Criação do projeto

---
+++ src/main/java/com/rezende/erdeliver/ErdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.rezende.erdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErdeliverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rezende/erdeliver/ErdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rezende.erdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
